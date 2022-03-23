f11ff9881f4bf317459650b4499c0f095e9c5f2e
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-23 16:04:16 -0700

    add Depends to load client in get_match route

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from docarray.document.pydantic_model import PydanticDocument, PydanticDocumentArray
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from docarray import Document, DocumentArray
 import cv2
 import numpy as np
-from typing import List
+from typing import List, Optional
 from docarray.array.sqlite import SqliteConfig
 from helpers import get_client, search_by_text, show_results, get_docs_from_sqlite
 from schemas import Item, MultipleItems
@@ -14,15 +14,14 @@ app = FastAPI()
 
 
 class Query(BaseModel):
-    query_text: str = "video games"
+    query_text: Optional[str] = "video games"
 
 @app.post('/get_match')
-def get_match(query: Query):
+def get_match(query: Query, client = Depends(get_client)):
     # NOTE: need to have create_data.py running in background for this to work
-    c = get_client()
-    query_text = query.query_text
-    results = search_by_text(c, query_text, verbose=True)
-    results = [[query_text, m.uri, m.scores['cosine'].value] for m in results[0].matches]
+    # NOTE: this needs to be run WITHOUT async in fapi b/c background grpc is using async loop
+    results = search_by_text(client, query.query_text, verbose=False)
+    results = [[query.query_text, m.uri, m.scores['cosine'].value] for m in results[0].matches]
     print(f'results: {results}')
     return {"results" : results}
 
